2db3eacecf673056d29b541e7c54d0a701330790
Anum Asif <anum@cockar.com>
2019-01-21 16:29:28 +0300

    Add a routing module to move between pages

---
+++ src/app/routing/routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes} from '@angular/router';
+import { MyprofileComponent} from '../myprofile/myprofile.component';
+import { UserComponent} from '../user/user.component'
+//Defining routes
+const routes:Routes=[
+  {path:"myprofile", component:MyprofileComponent},
+  {path:"user", component:UserComponent},
+  {path:"",redirectTo:"/myprofile",pathMatch:"full"}
+
+]
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
+})
+export class RoutingModule { }
