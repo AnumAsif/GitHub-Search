ae3066fab438b3e11e665fda55737d0f2e297482
Anum Asif <anum@cockar.com>
2019-01-21 16:37:59 +0300

    Save the required url with API key in the environment.ts file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,10 @@
 
 export const environment = {
   production: false,
-  apiUrl:"https://api.github.com/users/anumasif?access_token=56bd1895b2e987e016d5ec4a6edd9bd2d7addfda",
-  apiUrl2:"https://api.github.com/users/anumasif/repos?access_token=56bd1895b2e987e016d5ec4a6edd9bd2d7addfda"
+  apiUrla:"https://api.github.com/users/",
+  apiUrlb:"?access_token=56bd1895b2e987e016d5ec4a6edd9bd2d7addfda",
+  apiUrl2a:"https://api.github.com/users/",
+  apiUrl2b:"/repos?access_token=56bd1895b2e987e016d5ec4a6edd9bd2d7addfda"
 };
 
 /*
