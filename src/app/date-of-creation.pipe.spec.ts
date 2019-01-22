6c608c943f3743437c08a3bb057b2685fe975c8e
Anum Asif <anum@cockar.com>
2019-01-22 09:07:26 +0300

    Auto-generated commit

--- src/app/date-of-creation.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DateOfCreationPipe } from './date-of-creation.pipe';
-
-describe('DateOfCreationPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DateOfCreationPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/users.ts
+++
@@ -1,4 +0,0 @@
-import {User} from './user';
-export var Users:User[]=[
-
-]
